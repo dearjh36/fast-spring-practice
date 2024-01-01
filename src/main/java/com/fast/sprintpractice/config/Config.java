165f0de72db8b8de3e2647d93a7ad694b82a0aa1
juhee <dearjh36@naver.com>
2024-01-02 02:21:56 +0900

    Config #1 -  spring Configuration
    기초 설정
    루트 패키지 이하에 빈이 있는지 스캔

---
+++ src/main/java/com/fast/sprintpractice/config/Config.java
@@ -0,0 +1,10 @@
+package com.fast.sprintpractice.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@ComponentScan("com.fast.sprintpractice")
+@Configuration
+public class Config {
+
+}
